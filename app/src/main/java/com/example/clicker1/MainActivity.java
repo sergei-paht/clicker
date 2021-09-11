87872055d4e025d1fa41ad748b53fcf2c8b5b76c
sergei-paht <sergeu.packhtunov@mail.ru>
2021-09-11 13:19:44 +0300

    clicker

---
+++ app/src/main/java/com/example/clicker1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.clicker1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtnAddStudents(View view) {
+        counter++;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+    public void onClickBtnDellStudents(View view) {
+        counter = 0;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+}
